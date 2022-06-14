987eb1541f42f5b9271b12021bc78598f9cabb18
Kerry Buckley <kerryjbuckley@gmail.com>
2022-06-14 10:44:20 +0100

    Update database connection details

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,6 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :todo, Todo.Repo,
-  username: "postgres",
-  password: "postgres",
   hostname: "localhost",
   database: "todo_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
